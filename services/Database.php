a36600af49bc18c1091c7858004e4c62474d8ce8
Bhushan Vaiude <bhushan@highguts.com>
2017-06-04 16:40:40 +0530

    database changed added

--- services/Database.php
+++ services/Database.php
@@ -14,6 +14,7 @@ class Database {
 	@return Instance
 	*/
 	public static function getInstance() {
+
 		if(!self::$_instance) { // If no instance then make one
 			self::$_instance = new self();
 		}
@@ -21,6 +22,16 @@ class Database {
 	}
 	// Constructor
 	private function __construct() {
+		if($_SERVER['HTTP_HOST']!='tictock.local')
+		{
+			//mysql://b222d8833f4b8c:25ce1b5d@us-cdbr-iron-east-03.cleardb.net/heroku_af250eff4a217bc?reconnect=true
+			$this->_host = "us-cdbr-iron-east-03.cleardb.net";
+			$this->_username = "b222d8833f4b8c";
+			$this->_password = "25ce1b5d";
+			$this->_database = "heroku_af250eff4a217bc";
+			
+		}
+
 		$this->_connection = new mysqli($this->_host, $this->_username, 
 			$this->_password, $this->_database);
 	
